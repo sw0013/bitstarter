6a01c6c7e765383edff162f39ffa746b4d7d2fb9
sw0013 <spwarrier@yahoo.com>
2013-07-10 23:34:53 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer(16);
+  buffer = fs.readFileSync('./index.html');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
