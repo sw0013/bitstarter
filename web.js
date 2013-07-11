341e9e834aa757b0d0eb39e2c5bf92c31a63633b
sw0013 <spwarrier@yahoo.com>
2013-07-11 01:54:28 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -5,8 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buf = new buffer();
-  buf = fs.readFileSync('./index.html');
-  response.send(buf.toString(buf);
+    response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))
 });
 
 var port = process.env.PORT || 5000;
