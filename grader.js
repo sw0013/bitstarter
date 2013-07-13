c569c69de83581bddab149622d64d177b37b24cf
sw0013 <spwarrier@yahoo.com>
2013-07-13 23:14:23 +0000

    Edited grader.js

--- grader.js
+++ grader.js
@@ -27,6 +27,9 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var util = require('util');
+var rest = require('restler');
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,10 +68,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'Path to downloaded url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+  if (program.url) {
+      rest.get(program.url)
+            .on('complete', function(result){
+                var checkJson = checkHtmlFile(result, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson); 
+
+            });
+  }        
+  else if (program.file) {
+            
+              var checkJson = checkHtmlFile(program.file, program.checks);
+              var outJson = JSON.stringify(checkJson, null, 4);
+              console.log(outJson);
+  }
+  else {
     exports.checkHtmlFile = checkHtmlFile;
+  }
 }
